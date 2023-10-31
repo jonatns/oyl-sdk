50a0a13c6bd2f689e703cba8ff14c521f38ce59a
sprimage <timilehin1999@gmail.com>
2023-10-30 22:44:05 -0400

    Typedoc for api client

--- src.ts/apiclient/index.ts
+++ src.ts/apiclient/index.ts
@@ -1,150 +1,202 @@
-import fetch from 'node-fetch'
-import { SwapBrcBid, SignedBid } from '../shared/interface'
+import fetch from 'node-fetch';
+import { SwapBrcBid, SignedBid } from '../shared/interface';
 
+/**
+ * Represents the client for interacting with the Oyl API.
+ */
 export class OylApiClient {
-  private host: String
+  private host: string;
 
-  constructor(options?) {
-    try {
-      this.host = options.host
-    } catch (err) {
-      return err
-    }
+  /**
+   * Create an instance of the OylApiClient.
+   * @param options - Configuration object containing the API host.
+   */
+  constructor(options?: { host: string }) {
+    this.host = options?.host || '';
   }
 
-  static fromObject(data) {
-    const result = new this(data)
-    return result
+  /**
+   * Create an instance of the OylApiClient from a plain object.
+   * @param data - The data object.
+   * @returns An instance of OylApiClient.
+   */
+  static fromObject(data: { host: string }): OylApiClient {
+    return new this(data);
   }
 
-  toObject() {
+  /**
+   * Convert this OylApiClient instance to a plain object.
+   * @returns The plain object representation.
+   */
+  toObject(): { host: string } {
     return {
       host: this.host,
-    }
+    };
   }
 
-  async _call(path: string, method: string, data?: any) {
+  private async _call(path: string, method: string, data?: any) {
     try {
       const options: RequestInit = {
         method: method,
         headers: { 'Content-Type': 'application/json' }
+      };
+
+      if (['post', 'put', 'patch'].includes(method)) {
+        options.body = JSON.stringify(data);
       }
 
-      if (method === 'post' || method === 'put' || method === 'patch') {
-        options.body = JSON.stringify(data)
-      }
-
-      const response = await fetch(`${this.host + path}`, options)
-      const payload = await response.json()
-      return payload
+      const response = await fetch(`${this.host}${path}`, options);
+      return await response.json();
     } catch (err) {
-      return err
+      throw err;
     }
   }
 
-  async importAddress({ address }: { address: String }) {
-    return await this._call('/import-address', 'post', { address: address })
+  /**
+   * Import an address to the Oyl API.
+   * @param address - The address to be imported.
+   */
+  async importAddress({ address }: { address: string }): Promise<any> {
+    return await this._call('/import-address', 'post', { address });
   }
 
-  async pushTx({ transactionHex }: { transactionHex: String }) {
-    return await this._call('/broadcast-transaction', 'post', {
-      transactionHex: transactionHex,
-    })
+  /**
+   * Push a transaction.
+   * @param transactionHex - The hex of the transaction.
+   */
+  async pushTx({ transactionHex }: { transactionHex: string }): Promise<any> {
+    return await this._call('/broadcast-transaction', 'post', { transactionHex });
   }
 
-  async getTxByAddress(address: string) {
-    return await this._call('/address-transactions', 'post', {
-      address: address,
-    })
+  /**
+   * Get transactions by address.
+   * @param address - The address to query.
+   */
+  async getTxByAddress(address: string): Promise<any> {
+    return await this._call('/address-transactions', 'post', { address });
   }
 
-  async getBrc20sByAddress(address: string) {
-    return await this._call('/get-address-brc20-balance', 'post', {
-      address: address,
-    })
+  /**
+   * Get Brc20 balances by address.
+   * @param address - The address to query.
+   */
+  async getBrc20sByAddress(address: string): Promise<any> {
+    return await this._call('/get-address-brc20-balance', 'post', { address });
   }
 
-  async getCollectiblesById(id: string) {
-    return await this._call('/get-inscription-info', 'post', {
-      inscription_id: id,
-    })
+  /**
+   * Get collectible by ID.
+   * @param id - The ID of the collectible.
+   */
+  async getCollectiblesById(id: string): Promise<any> {
+    return await this._call('/get-inscription-info', 'post', { inscription_id: id });
   }
 
-  async getCollectiblesByAddress(address: string) {
+  /**
+   * Get collectibles by address.
+   * @param address - The address to query.
+   */
+  async getCollectiblesByAddress(address: string): Promise<any> {
     return await this._call('/get-inscriptions', 'post', {
-      address: address,
-      exclude_brc20: true,
-    })
-  }
-
-  async listWallet() {
-    return await this._call('/list-wallets', 'get')
-  }
-
-  async listTx() {
-    return await this._call('/list-tx', 'get')
-  }
-
-  async getRawMempool() {
-    return await this._call('/mempool', 'get')
-  }
-
-  async getMempoolInfo() {
-    return await this._call('/mempool-info', 'get')
-  }
-
-  async getTickerOffers({ _ticker }: { _ticker: String }) {
-    const response = await this._call('/get-token-offers', 'post', {
-      ticker: _ticker,
-    })
-    const list = response.data.list
-    return list
-  }
-
-  async initSwapBid({ address, auctionId, bidPrice, pubKey }: SwapBrcBid) {
-    return await this._call('/initiate-bid', 'post', {
       address,
-      auctionId,
-      bidPrice,
-      pubKey,
-    })
+      exclude_brc20: true,
+    });
   }
 
-  async submitSignedBid({ psbtBid, auctionId, bidId }: SignedBid) {
-    return await this._call('/finalize-bid', 'post', {
-      psbtBid,
-      auctionId,
-      bidId,
-    })
+  /**
+   * List wallets.
+   */
+  async listWallet(): Promise<any> {
+    return await this._call('/list-wallets', 'get');
   }
 
-  async getFees() {
-    return await this._call('/get-fees', 'get')
+  /**
+   * List transactions.
+   */
+  async listTx(): Promise<any> {
+    return await this._call('/list-tx', 'get');
   }
 
+  /**
+   * Get raw mempool.
+   */
+  async getRawMempool(): Promise<any> {
+    return await this._call('/mempool', 'get');
+  }
+
+  /**
+   * Get mempool information.
+   */
+  async getMempoolInfo(): Promise<any> {
+    return await this._call('/mempool-info', 'get');
+  }
+
+  /**
+   * Get ticker offers.
+   * @param _ticker - The ticker to query.
+   */
+  async getTickerOffers({ _ticker }: { _ticker: string }): Promise<any> {
+    const response = await this._call('/get-token-offers', 'post', { ticker: _ticker });
+    return response.data.list;
+  }
+
+  /**
+   * Initialize a swap bid.
+   * @param params - Parameters for the bid.
+   */
+  async initSwapBid(params: SwapBrcBid): Promise<any> {
+    return await this._call('/initiate-bid', 'post', params);
+  }
+
+  /**
+   * Submit a signed bid.
+   * @param params - Parameters for the signed bid.
+   */
+  async submitSignedBid(params: SignedBid): Promise<any> {
+    return await this._call('/finalize-bid', 'post', params);
+  }
+
+  /**
+   * Get transaction fees.
+   */
+  async getFees(): Promise<any> {
+    return await this._call('/get-fees', 'get');
+  }
+
+  /**
+   * Subscribe for notifications.
+   * @param webhookUrl - The URL to send notifications.
+   * @param rbf - Replace-by-fee flag.
+   */
   async subscribe({
     webhookUrl,
     rbf = false,
   }: {
-    webhookUrl: String
-    rbf?: Boolean
-  }) {
+    webhookUrl: string;
+    rbf?: boolean;
+  }): Promise<any> {
     return await this._call('/subscribe-webhook', 'post', {
-      webhookUrl: webhookUrl,
-      rbf: rbf,
-    })
+      webhookUrl,
+      rbf,
+    });
   }
 
+  /**
+   * Import an address and subscribe for notifications.
+   * @param address - The address to be imported.
+   * @param webhookUrl - The URL to send notifications.
+   * @param rbf - Replace-by-fee flag.
+   */
   async importSubscribe({
     address,
     webhookUrl,
     rbf,
   }: {
-    address: String
-    webhookUrl: String
-    rbf?: Boolean
-  }) {
-    await this.importAddress({ address })
-    await this.subscribe({ webhookUrl, rbf })
+    address: string;
+    webhookUrl: string;
+    rbf?: boolean;
+  }): Promise<void> {
+    await this.importAddress({ address });
+    await this.subscribe({ webhookUrl, rbf });
   }
 }
