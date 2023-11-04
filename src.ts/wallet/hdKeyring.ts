5e8344baa37cc48e35f0943bdb8f9ec2f1a3f39a
sprimage <timilehin1999@gmail.com>
2023-11-04 14:11:35 -0600

    hdKeyring typedoc annotations

--- src.ts/wallet/hdKeyring.ts
+++ src.ts/wallet/hdKeyring.ts
@@ -26,11 +26,19 @@ export class HdKeyring extends EventEmitter {
   private _index2wallet: Record<number, [string, ECPairInterface]> = {}
   activeIndexes: number[] = []
 
+  /**
+   * Initializes a new instance of the HdKeyring class.
+   * @param {HDKeyringOption} opts - HD Keyring options.
+   */
   constructor(opts?: HDKeyringOption) {
     super(null)
     this.deserialize(opts)
   }
 
+  /**
+   * Serializes the HDKeyring instance to a JSON object.
+   * @returns {Promise<HDKeyringOption>} A promise that resolves to an object with the HDKeyring properties.
+   */
   async serialize(): Promise<HDKeyringOption> {
     return {
       mnemonic: this.mnemonic,
@@ -40,7 +48,12 @@ export class HdKeyring extends EventEmitter {
     }
   }
 
-  deserialize(_opts: HDKeyringOption = {}) {
+  /**
+  * Deserializes options to a HDKeyring instance.
+  * @param {HDKeyringOption} _opts - The HDKeyring options object.
+  * @returns {HdKeyring} The instance of the HDKeyring.
+  */
+  deserialize(_opts: HDKeyringOption = {}): HdKeyring {
     if (this.root) {
       throw new Error('Btc-Hd-Keyring: Secret recovery phrase already provided')
     }
@@ -65,6 +78,10 @@ export class HdKeyring extends EventEmitter {
     return this
   }
 
+  /**
+ * Initializes the HD keyring from a mnemonic phrase.
+ * @param {string} mnemonic - The mnemonic phrase to use for initialization.
+ */
   initFromMnemonic(mnemonic: string) {
     if (this.root) {
       throw new Error('Btc-Hd-Keyring: Secret recovery phrase already provided')
@@ -83,6 +100,10 @@ export class HdKeyring extends EventEmitter {
     //console.log("root", this.root)
   }
 
+  /**
+  * Changes the HD path used by the keyring and reinitializes accounts.
+  * @param {string} hdPath - The new HD path to be used.
+  */
   changeHdPath(hdPath: string) {
     this.hdPath = hdPath
 
@@ -100,6 +121,12 @@ export class HdKeyring extends EventEmitter {
     this.activeAccounts(indexes)
   }
 
+  /**
+   * Retrieves an account's address by its HD path and index.
+   * @param {string} hdPath - The HD path to derive the account from.
+   * @param {number} index - The index of the account.
+   * @returns {string} The account address as a string.
+   */
   getAccountByHdPath(hdPath: string, index: number) {
     const root = this.hdWallet
       .toHDPrivateKey(
@@ -113,6 +140,11 @@ export class HdKeyring extends EventEmitter {
     return address
   }
 
+  /**
+  * Adds a specified number of new accounts to the keyring.
+  * @param {number} numberOfAccounts - The number of new accounts to add. Defaults to 1 if not specified.
+  * @returns {Promise<string[]>} A promise that resolves to an array of new account addresses in hex format.
+  */
   addAccounts(numberOfAccounts = 1) {
     if (!this.root) {
       this.initFromMnemonic(new Mnemonic().toString())
@@ -145,6 +177,11 @@ export class HdKeyring extends EventEmitter {
     return Promise.resolve(hexWallets)
   }
 
+  /**
+   * Activates a list of accounts by their indexes.
+   * @param {number[]} indexes - An array of account indexes to activate.
+   * @returns An array of activated account addresses in hex format.
+   */
   activeAccounts(indexes: number[]) {
     const accounts: string[] = []
     for (const index of indexes) {
@@ -158,6 +195,12 @@ export class HdKeyring extends EventEmitter {
     return accounts
   }
 
+  /**
+   * Retrieves a list of addresses within a specified index range.
+   * @param {number} start - The start index of the range.
+   * @param {number} end - The end index of the range.
+   * @returns {{ address: string; index: number }[]} An array of objects with address and index properties.
+   */
   getAddresses(start: number, end: number) {
     const from = start
     const to = end
@@ -172,12 +215,22 @@ export class HdKeyring extends EventEmitter {
     return accounts
   }
 
+  /**
+  * Gets the hex string representations of public keys for all accounts.
+  * @returns {string[]} An array of account addresses in hex format.
+  */
   getAccounts() {
     return this.wallets.map((w) => {
       return w.publicKey.toString('hex')
     })
   }
 
+  /**
+  * Retrieves the private key for the given public key.
+  * @param {string} publicKey - The public key to retrieve the private key for.
+  * @returns {ECPairInterface} The corresponding private key.
+  * @private
+  */
   private _getPrivateKeyFor(publicKey: string) {
     if (!publicKey) {
       throw new Error('Must specify publicKey.')
@@ -186,6 +239,12 @@ export class HdKeyring extends EventEmitter {
     return wallet
   }
 
+  /**
+   * Retrieves the wallet for a given account's public key.
+   * @param {string} publicKey - The public key of the account to retrieve the wallet for.
+   * @returns {ECPairInterface} The wallet corresponding to the given public key.
+   * @private
+   */
   private _getWalletForAccount(publicKey: string) {
     let wallet = this.wallets.find(
       (wallet) => wallet.publicKey.toString('hex') == publicKey
@@ -196,6 +255,13 @@ export class HdKeyring extends EventEmitter {
     return wallet
   }
 
+  /**
+  * Signs a PSBT (Partially Signed Bitcoin Transaction) using the private keys managed by this keyring.
+  * @param {bitcoin.Psbt} psbt - The PSBT to sign.
+  * @param {{ index: number; publicKey: string; sighashTypes?: number[] }[]} inputs - The inputs to sign, with their index, public key, and optional sighash types.
+  * @param opts - Additional options.
+  * @returns {Promise<bitcoin.Psbt>} A promise that resolves to the signed PSBT.
+  */
   async signTransaction(
     psbt: bitcoin.Psbt,
     inputs: { index: number; publicKey: string; sighashTypes?: number[] }[],
@@ -211,8 +277,8 @@ export class HdKeyring extends EventEmitter {
 
         const signer =
           input.witnessUtxo?.script &&
-          input.tapInternalKey &&
-          !input.tapLeafScript
+            input.tapInternalKey &&
+            !input.tapLeafScript
             ? tweakedSigner
             : keyPair
 
@@ -224,6 +290,11 @@ export class HdKeyring extends EventEmitter {
     return psbt
   }
 
+  /**
+  * Retrieves the address and corresponding ECPair object from a given index.
+  * @param {number} i - The index to derive the address from.
+  * @returns {[string, ECPairInterface]} A tuple containing the address and the ECPair object.
+  */
   private _addressFromIndex(i: number): [string, ECPairInterface] {
     if (!this._index2wallet[i]) {
       const child = this.root!.deriveChild(i)
