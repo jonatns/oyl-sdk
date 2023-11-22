e88859a050961f4a0fbd5c4f3cba53000e5cffd1
sprimage <timilehin1999@gmail.com>
2023-11-22 17:05:42 -0600

    esplora methods

--- lib/rpclient/esplora.d.ts
+++ lib/rpclient/esplora.d.ts
@@ -2,4 +2,12 @@ export declare class EsploraRpc {
     esploraUrl: string;
     constructor(url: string);
     _call(method: any, params?: any[]): Promise<any>;
+    getTxInfo(txid: string): Promise<any>;
+    getTxStatus(txid: string): Promise<any>;
+    getTxHex(txid: string): Promise<any>;
+    getTxRaw(txid: string): Promise<any>;
+    getTxOutspends(txid: string): Promise<any>;
+    getAddressTx(address: string): Promise<any>;
+    getAddressUtxo(address: string): Promise<any>;
+    getFeeEstimates(): Promise<any>;
 }
--- src.ts/rpclient/esplora.ts
+++ src.ts/rpclient/esplora.ts
@@ -38,4 +38,36 @@ export class EsploraRpc {
             throw error;
         }
     }
+
+    async getTxInfo (txid: string) {
+        return await this._call("esplora_tx", [txid])
+    }
+
+    async getTxStatus (txid: string) {
+        return await this._call("esplora_tx::status", [txid])
+    }
+
+    async getTxHex (txid: string) {
+        return await this._call("esplora_tx::hex", [txid])
+    }
+
+    async getTxRaw (txid: string) {
+        return await this._call("esplora_tx::raw", [txid])
+    }
+
+    async getTxOutspends (txid: string) {
+        return await this._call("esplora_tx::outspends", [txid])
+    }
+
+    async getAddressTx (address: string) {
+        return await this._call("esplora_address::txs", [address])
+    }
+
+    async getAddressUtxo (address: string) {
+        return await this._call("esplora_address::utxo", [address])
+    }
+    async getFeeEstimates () {
+        return await this._call("esplora_fee-estimates")
+    }
+
 }
