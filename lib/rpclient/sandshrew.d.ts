c55bcea37d9c01742bab53067073241b015f1f4c
sprimage <timilehin1999@gmail.com>
2023-11-22 11:28:29 -0600

    initialize relevant rpc methods and their types

--- lib/rpclient/sandshrew.d.ts
+++ lib/rpclient/sandshrew.d.ts
@@ -2,6 +2,7 @@ export declare class SandshrewBitcoinClient {
     apiUrl: string;
     constructor(apiUrl: any);
     _call(method: any, params?: any[]): Promise<any>;
+    _initializeRpcMethods(): void;
     _createRpcMethod(methodName: any, argType: any): void;
     _convertArg(arg: any, argType: any): any;
 }
