9a6f9ca2a038a44c6c43d7f7cb4edcd4159f5b91
Jonathan P. Navarrete <jonatns@gmail.com>
2023-11-30 21:14:58 -0800

    Fix modules on postinstall

--- scripts/fix-modules.js
+++ scripts/fix-modules.js
@@ -1,52 +1,14 @@
-const fs = require('fs');
+const fs = require('fs')
 const fixWindowError = () => {
-  const file = './node_modules/bitcore-lib/lib/crypto/random.js';
-  let fileData = fs.readFileSync(file).toString();
-  fileData = fileData.replace(
-    `Random.getRandomBufferBrowser = function(size) {
-  if (!window.crypto && !window.msCrypto)
-    throw new Error('window.crypto not available');
-  if (window.crypto && window.crypto.getRandomValues)
-    var crypto = window.crypto;
-  else if (window.msCrypto && window.msCrypto.getRandomValues) //internet explorer
-    var crypto = window.msCrypto;
-  else
-    throw new Error('window.crypto.getRandomValues not available');
-  var bbuf = new Uint8Array(size);
-  crypto.getRandomValues(bbuf);
-  var buf = Buffer.from(bbuf);
-  return buf;
-};`,
-    `Random.getRandomBufferBrowser = function(size) {
-  var bbuf = new Uint8Array(size);
-  crypto.getRandomValues(bbuf);
-  var buf = Buffer.from(bbuf);
-  return buf;
-};`
-  );
-  fs.writeFileSync(file, fileData);
-};
-
-const fixWindowError2 = () => {
-  const file = './node_modules/tiny-secp256k1/lib/rand.browser.js';
-  let fileData = fs.readFileSync(file).toString();
-  fileData = fileData.replace('window.crypto', 'crypto');
-  fs.writeFileSync(file, fileData);
-};
-
-const fixWindowError3 = () => {
-  const file = './node_modules/bitcoinjs-lib/src/payments/p2tr.js';
-  let fileData = fs.readFileSync(file).toString();
-  fileData = fileData.replace(
-    'signature: types_1.typeforce.maybe(types_1.typeforce.BufferN(64))',
-    'signature: types_1.typeforce.maybe(types_1.typeforce.Buffer)'
-  );
-  fs.writeFileSync(file, fileData);
-};
+  const file = './node_modules/bitcore-lib/lib/crypto/random.js'
+  let fileData = fs.readFileSync(file).toString()
+  fileData = fileData.replace('process.browser', 'false')
+  fs.writeFileSync(file, fileData)
+}
 
 const fixBufferError = () => {
-  const file = './node_modules/bitcore-lib/lib/crypto/signature.js';
-  let fileData = fs.readFileSync(file).toString();
+  const file = './node_modules/bitcore-lib/lib/crypto/signature.js'
+  let fileData = fs.readFileSync(file).toString()
   fileData = fileData.replace(
     `var Signature = function Signature(r, s) {
   if (!(this instanceof Signature)) {
@@ -78,23 +40,21 @@ const fixBufferError = () => {
   this.r = BN.fromString(this.r.toString(16), 16)
   this.s = BN.fromString(this.s.toString(16),16)
 };`
-  );
-  fs.writeFileSync(file, fileData);
-};
+  )
+  fs.writeFileSync(file, fileData)
+}
 
 const run = async () => {
-  let success = true;
+  let success = true
   try {
-    fixWindowError();
-    fixWindowError2();
-    fixWindowError3();
-    fixBufferError();
+    fixWindowError()
+    fixBufferError()
   } catch (e) {
-    console.error('error:', e.message);
-    success = false;
+    console.error('error:', e.message)
+    success = false
   } finally {
-    console.log('Fix modules result: ', success ? 'success' : 'failed');
+    console.log('Fix modules result:', success ? 'success' : 'failed')
   }
-};
+}
 
-run();
+run()
