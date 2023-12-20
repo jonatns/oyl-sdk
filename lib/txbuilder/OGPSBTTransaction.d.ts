5a880cdcc2dd6721004fff7046024b3ac94b003d
sprimage <timilehin1999@gmail.com>
2023-12-19 20:35:48 -0600

    OG psbt transaction

---
+++ lib/txbuilder/OGPSBTTransaction.d.ts
@@ -0,0 +1,41 @@
+import * as bitcoin from 'bitcoinjs-lib';
+import { AddressType, UnspentOutput, TxOutput, ToSignInput } from '../shared/interface';
+export declare class OGPSBTTransaction {
+    private inputs;
+    outputs: TxOutput[];
+    private changeOutputIndex;
+    private signer;
+    private address;
+    changedAddress: string;
+    private network;
+    private feeRate;
+    private pubkey;
+    private addressType;
+    private enableRBF;
+    constructor(signer: any, address: string, pubkey: string, addressType: AddressType, feeRate?: number);
+    setEnableRBF(enable: boolean): void;
+    setChangeAddress(address: string): void;
+    addInput(utxo: UnspentOutput): void;
+    getTotalInput(): number;
+    getTotalOutput(): number;
+    getUnspent(): number;
+    isEnoughFee(): Promise<boolean>;
+    calNetworkFee(): Promise<number>;
+    addOutput(address: string, value: number): void;
+    getOutput(index: number): TxOutput;
+    addChangeOutput(value: number): void;
+    getChangeOutput(): TxOutput;
+    getChangeAmount(): number;
+    removeChangeOutput(): void;
+    removeRecentOutputs(count: number): void;
+    formatOptionsToSignInputs: (_psbt: string | bitcoin.Psbt, isRevealTx?: boolean) => Promise<ToSignInput[]>;
+    createSignedPsbt(): Promise<bitcoin.Psbt>;
+    signPsbt(psbt: bitcoin.Psbt, autoFinalized?: boolean, isRevealTx?: boolean): Promise<bitcoin.Psbt>;
+    generate(autoAdjust: boolean): Promise<{
+        fee: number;
+        rawtx: string;
+        toSatoshis: number;
+        estimateFee: number;
+    }>;
+    dumpTx(psbt: any): Promise<void>;
+}
