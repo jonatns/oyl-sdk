d379ec7505eebc239f28fdbbd7bb7e4bc44cb877
sprimage <timilehin1999@gmail.com>
2024-04-02 17:29:22 -0600

    return base64

--- src.ts/marketplace/index.ts
+++ src.ts/marketplace/index.ts
@@ -443,12 +443,12 @@ export class Marketplace {
       const keyPair = this.signer.segwitKeyPair;
       const privateKey = keyPair.toWIF()
       const signature = bip322Signer.sign( privateKey, this.receiveAddress, message); 
-      return signature.toString('hex')
+      return signature.toString('base64')
     } else if (getAddressType(this.receiveAddress) == AddressType.P2TR){
       const keyPair =  this.signer.taprootKeyPair;
       const privateKey = keyPair.toWIF()
       const signature = bip322Signer.sign( privateKey, this.receiveAddress, message); 
-      return signature.toString('hex')
+      return signature.toString('base64')
     }
   }
 }
