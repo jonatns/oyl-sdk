798189d127cc8693e7c5342d6d1a2beca9973340
phuncti0n <zmill777@proton.me>
2024-05-30 12:11:10 -0400

    provider fix

--- lib/provider/provider.d.ts
+++ lib/provider/provider.d.ts
@@ -9,10 +9,11 @@ export declare class Provider {
     ord: OrdRpc;
     api: OylApiClient;
     network: bitcoin.networks.Network;
-    constructor({ url, projectId, network, version, }: {
+    constructor({ url, projectId, network, networkType, version, }: {
         url: string;
         projectId: string;
         network: bitcoin.networks.Network;
+        networkType: 'signet' | 'mainnet' | 'testnet';
         version?: string;
     });
 }
