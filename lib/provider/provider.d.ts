b15fd9e06b3b202b429a6da8709d51a8d1264581
phuncti0n <zmill777@proton.me>
2024-07-08 18:03:53 -0400

    fix

--- lib/provider/provider.d.ts
+++ lib/provider/provider.d.ts
@@ -3,19 +3,20 @@ import { EsploraRpc } from '../rpclient/esplora';
 import { OrdRpc } from '../rpclient/ord';
 import { OylApiClient } from '../apiclient';
 import * as bitcoin from 'bitcoinjs-lib';
+export type ProviderConstructorArgs = {
+    url: string;
+    projectId: string;
+    network: bitcoin.networks.Network;
+    networkType: 'signet' | 'mainnet' | 'testnet';
+    version?: string;
+};
 export declare class Provider {
     sandshrew: SandshrewBitcoinClient;
     esplora: EsploraRpc;
     ord: OrdRpc;
     api: OylApiClient;
     network: bitcoin.networks.Network;
-    constructor({ url, projectId, network, networkType, version, }: {
-        url: string;
-        projectId: string;
-        network: bitcoin.networks.Network;
-        networkType: 'signet' | 'mainnet' | 'testnet';
-        version?: string;
-    });
+    constructor({ url, projectId, network, networkType, version, }: ProviderConstructorArgs);
     pushPsbt({ psbtHex, psbtBase64, }: {
         psbtHex?: string;
         psbtBase64?: string;
