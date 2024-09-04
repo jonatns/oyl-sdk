bc96ffb8de41e5263f5d96d163678a6c9098f2bf
sprimage <timilehin1999@gmail.com>
2024-09-04 16:19:12 -0600

    build

---
+++ src/swap/swap.ts
@@ -0,0 +1,16 @@
+import { unisatSwap } from './unisat/unisat'
+import { okxSwap } from './okx/okx'
+import { Marketplaces, ProcessOfferOptions, SwapResponse, marketplaceName } from './types'
+import { ordinalWalletSwap } from './ordinals-wallet/ordinals-wallet'
+
+
+export async function processOffer (options: ProcessOfferOptions): Promise<SwapResponse>{
+    switch (marketplaceName[options.offer.marketplace]){
+        case Marketplaces.UNISAT:
+            return await unisatSwap(options);
+        case Marketplaces.ORDINALS_WALLET:
+            return await ordinalWalletSwap(options);
+        case Marketplaces.OKX:
+            return await okxSwap(options);
+    }
+}
