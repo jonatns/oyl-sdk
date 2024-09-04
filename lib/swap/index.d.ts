527d6130353377033803a279ae6a96414b4acd41
sprimage <timilehin1999@gmail.com>
2024-09-04 17:44:01 -0600

    helper, types, swap exports

--- lib/swap/index.d.ts
+++ lib/swap/index.d.ts
@@ -1,3 +1,6 @@
 export * as unisat from './unisat';
 export * as ordinalsWallet from './ordinals-wallet';
 export * as okx from './okx';
+export * as swap from './swap';
+export * as helpers from './helpers';
+export * as types from './types';
