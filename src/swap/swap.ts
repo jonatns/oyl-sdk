52d22a3627c8d55012ed899dd366c94d781cc448
Matt Abrams (mcabrams) <1213498+mcabrams@users.noreply.github.com>
2024-09-05 16:13:35 -0400

    fix import

--- src/swap/swap.ts
+++ src/swap/swap.ts
@@ -1,27 +1,33 @@
 import { unisatSwap } from './unisat/unisat'
 import { okxSwap } from './okx/okx'
-import { Marketplaces, ProcessOfferOptions, SwapResponse, marketplaceName } from './types'
+import {
+  Marketplaces,
+  ProcessOfferOptions,
+  SwapResponse,
+  marketplaceName,
+} from './types'
 import { ordinalWalletSwap } from './ordinals-wallet/ordinals-wallet'
-import { waitForTransaction } from 'shared/utils'
+import { waitForTransaction } from '../shared/utils'
 
+export async function processOffer(
+  options: ProcessOfferOptions
+): Promise<SwapResponse> {
+  let swapResponse: SwapResponse
+  switch (marketplaceName[options.offer.marketplace]) {
+    case Marketplaces.UNISAT:
+      swapResponse = await unisatSwap(options)
+      break
+    case Marketplaces.ORDINALS_WALLET:
+      swapResponse = await ordinalWalletSwap(options)
+      break
+    case Marketplaces.OKX:
+      swapResponse = await okxSwap(options)
+      break
+  }
+  await waitForTransaction({
+    txId: swapResponse.purchaseTxId,
+    sandshrewBtcClient: this.sandshrew,
+  })
 
-export async function processOffer (options: ProcessOfferOptions): Promise<SwapResponse>{
-    let swapResponse: SwapResponse
-    switch (marketplaceName[options.offer.marketplace]){
-        case Marketplaces.UNISAT:
-            swapResponse = await unisatSwap(options);
-            break;
-        case Marketplaces.ORDINALS_WALLET:
-            swapResponse = await ordinalWalletSwap(options);
-            break
-        case Marketplaces.OKX:
-            swapResponse = await okxSwap(options);
-            break;
-    }
-    await waitForTransaction({
-        txId: swapResponse.purchaseTxId,
-        sandshrewBtcClient: this.sandshrew,
-      })
-
-    return swapResponse
+  return swapResponse
 }
