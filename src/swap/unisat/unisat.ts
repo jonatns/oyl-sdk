802c542c4f85f58a12f2a3cee2c90d107384ab87
sprimage <timilehin1999@gmail.com>
2024-10-30 12:13:34 -0600

    sign unisat dummy psbt

--- src/swap/unisat/unisat.ts
+++ src/swap/unisat/unisat.ts
@@ -6,122 +6,130 @@ import { signBip322Message } from "./BIP322"
 import { ProcessOfferOptions, SwapResponse } from "../types"
 
 export interface UnsignedUnisatBid {
-    address: string
-    auctionId: string | string[]
-    bidPrice: number | number[]
-    pubKey: string
-    receiveAddress: string
-    altAddressSignature?: string
-    feerate: number
-    provider: Provider
-    assetType: AssetType
-  }
+  address: string
+  auctionId: string | string[]
+  bidPrice: number | number[]
+  pubKey: string
+  receiveAddress: string
+  altAddressSignature?: string
+  feerate: number
+  provider: Provider
+  assetType: AssetType
+}
 
-  export interface SignedUnisatBid {
-    psbtHex: string
-    auctionId: string
-    bidId: string
-    provider: Provider
-    assetType: AssetType
-  }
+export interface SignedUnisatBid {
+  psbtHex: string
+  auctionId: string
+  bidId: string
+  provider: Provider
+  assetType: AssetType
+}
 
 export async function getPsbt(unsignedBid: UnsignedUnisatBid) {
-    switch (unsignedBid.assetType) {
-      case AssetType.BRC20:
-        return await unsignedBid.provider.api.initSwapBid(unsignedBid)
+  switch (unsignedBid.assetType) {
+    case AssetType.BRC20:
+      return await unsignedBid.provider.api.initSwapBid(unsignedBid)
 
-      case AssetType.RUNES:
-        return await unsignedBid.provider.api.initRuneSwapBid(unsignedBid)
+    case AssetType.RUNES:
+      return await unsignedBid.provider.api.initRuneSwapBid(unsignedBid)
 
-      case AssetType.COLLECTIBLE:
-        return await unsignedBid.provider.api.initCollectionSwapBid(unsignedBid)
-    }
+    case AssetType.COLLECTIBLE:
+      return await unsignedBid.provider.api.initCollectionSwapBid(unsignedBid)
   }
+}
 
- export async function submitPsbt(signedBid: SignedUnisatBid) {
-    switch (signedBid.assetType) {
-      case AssetType.BRC20:
-        return await signedBid.provider.api.submitSignedBid({...signedBid, psbtBid: signedBid.psbtHex})
+export async function submitPsbt(signedBid: SignedUnisatBid) {
+  switch (signedBid.assetType) {
+    case AssetType.BRC20:
+      return await signedBid.provider.api.submitSignedBid({ ...signedBid, psbtBid: signedBid.psbtHex })
 
-      case AssetType.RUNES:
-        return await signedBid.provider.api.submitSignedRuneBid({...signedBid, psbtBid: signedBid.psbtHex})
+    case AssetType.RUNES:
+      return await signedBid.provider.api.submitSignedRuneBid({ ...signedBid, psbtBid: signedBid.psbtHex })
 
-      case AssetType.COLLECTIBLE:
-        return await signedBid.provider.api.submitSignedCollectionBid({...signedBid, psbtBid: signedBid.psbtHex})
+    case AssetType.COLLECTIBLE:
+      return await signedBid.provider.api.submitSignedCollectionBid({ ...signedBid, psbtBid: signedBid.psbtHex })
 
-    }
   }
+}
 
-  export async function unisatSwap ({
-    address, 
-    offer,
-    receiveAddress,
-    feeRate,
-    pubKey,
-    assetType,
-    provider,
-    utxos,
-    signer
+export async function unisatSwap({
+  address,
+  offer,
+  receiveAddress,
+  feeRate,
+  pubKey,
+  assetType,
+  provider,
+  utxos,
+  signer
 }: ProcessOfferOptions
 ): Promise<SwapResponse> {
-    let dummyTxId: string | null = null;
-    let purchaseTxId: string | null = null;
-    const unsignedBid: UnsignedUnisatBid = {
-      address,
-      auctionId: offer.offerId,
-      bidPrice: offer.totalPrice,
-      pubKey,
-      receiveAddress,
-      feerate: feeRate,
-      provider,
-      assetType
-    }
-    if (
-      address != receiveAddress 
-    ) {
-      const signature = await getMessageSignature({address, provider, receiveAddress, signer})
-      unsignedBid['signature'] = signature
-    }
-    const psbt_ = await getPsbt(unsignedBid)
-
-    if (psbt_?.error) {
-      throw new OylTransactionError(psbt_?.error)
-    }
-      const unsignedPsbt: string = psbt_.psbtBid
-      const signedPsbt = await signer.signAllInputs({
-        rawPsbtHex: unsignedPsbt,
-        finalize: false,
-    })
-    const data = await submitPsbt({
-        psbtHex: signedPsbt.signedHexPsbt,
-        auctionId: offer.offerId,
-        bidId: psbt_.bidId,
-        assetType,
-        provider
-    })
-    if (data.txid) {
-      purchaseTxId = data.txid
-    }
-
-    return {
-      dummyTxId,
-      purchaseTxId
-    }
-   }
-
-    
-  export async function getMessageSignature({
+  let dummyTxId: string | null = null;
+  let purchaseTxId: string | null = null;
+  const unsignedBid: UnsignedUnisatBid = {
     address,
+    auctionId: offer.offerId,
+    bidPrice: offer.totalPrice,
+    pubKey,
     receiveAddress,
-    signer,
+    feerate: feeRate,
+    provider,
+    assetType
+  }
+  if (
+    address != receiveAddress
+  ) {
+    const signature = await getMessageSignature({ address, provider, receiveAddress, signer })
+    unsignedBid['signature'] = signature
+  }
+  const psbt_ = await getPsbt(unsignedBid)
+
+  if (psbt_?.error) {
+    throw new OylTransactionError(psbt_?.error)
+  }
+  if (psbt_.psbtDummy) {
+    const unsignedDummyPsbt = psbt_.psbtDummy
+    const signedDummyPsbt = await signer.signAllInputs({
+      rawPsbtHex: unsignedDummyPsbt, 
+      finalize: true,
+    })
+    await provider.pushPsbt({ psbtBase64: signedDummyPsbt.signedPsbt })
+  }
+  const unsignedPsbt: string = psbt_.psbtBid
+  const signedPsbt = await signer.signAllInputs({
+    rawPsbtHex: unsignedPsbt,
+    finalize: false,
+  })
+  const data = await submitPsbt({
+    psbtHex: signedPsbt.signedHexPsbt,
+    auctionId: offer.offerId,
+    bidId: psbt_.bidId,
+    assetType,
     provider
-  }: {
-    address: string,
-    receiveAddress: string,
-    signer: Signer,
-    provider: Provider
-  }): Promise<string> {
-     const message = `Please confirm that\nPayment Address: ${address}\nOrdinals Address: ${receiveAddress}`
+  })
+  if (data.txid) {
+    purchaseTxId = data.txid
+  }
+
+  return {
+    dummyTxId,
+    purchaseTxId
+  }
+}
+
+
+export async function getMessageSignature({
+  address,
+  receiveAddress,
+  signer,
+  provider
+}: {
+  address: string,
+  receiveAddress: string,
+  signer: Signer,
+  provider: Provider
+}): Promise<string> {
+  const message = `Please confirm that\nPayment Address: ${address}\nOrdinals Address: ${receiveAddress}`
   if (getAddressType(receiveAddress) == AddressType.P2WPKH) {
     const keyPair = signer.segwitKeyPair
     const privateKey = keyPair.privateKey
