7b535e28302e5747a4a2d94a80d44ccf686fdaf1
Eric Butz <ericgbutz@gmail.com>
2025-04-21 09:26:01 -0600

    Add cli to generate addresses from mnemonic

--- lib/cli/account.d.ts
+++ lib/cli/account.d.ts
@@ -3,3 +3,10 @@ export declare const privateKeysCommand: Command;
 export declare const mnemonicToAccountCommand: Command;
 export declare const generateMnemonicCommand: Command;
 export declare const signPsbt: Command;
+/**
+ * @dev example call
+ * oyl account generateAddresses -p bitcoin -n 10
+ *
+ * You will need to specify a MNOMONIC= in your .env file
+ */
+export declare const generateAddressesCommand: Command;
