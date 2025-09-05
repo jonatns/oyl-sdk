e230b62ff49e1e7fedf4ddd11911f2ffe908aa0a
Hude Intres <nodadfat@gmail.com>
2025-09-04 23:56:08 -0500

    fix tests

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/src/**/*.(spec|test).[jt]s?(x)'],
+  modulePathIgnorePatterns: ['<rootDir>/lib/'],
 }
 
 export default config
